ef1e99611b2ac65488b522f4978933d2794c1366
pedro-githube <pedrobittetadeveloper@hotmail.com>
2022-03-13 02:32:04 -0300

    Fixing imports for netlify

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,5 +1,4 @@
 import axios, { AxiosRequestConfig} from 'axios';
-import { eventNames } from 'process';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
